2f5c250573a859f6df138e8c3d27eb906d0b8493
Ilvian <d.ilvian1993@gmail.com>
2023-06-16 04:27:46 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
-    printf("Size of a char: 1 byte(s)\n", sizeof(intType));
-    printf("Size of an int: 4 byte(s)\n", sizeof(floatType));
-    printf("Size of a long int: 4 byte(s)\n", sizeof(floatType));
-    printf("Size of a long long int: 8 byte(s)\n", sizeof(doubleType));
-    printf("Size of a float: 4 byte(s)\n", sizeof(charType));
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
     return 0;
 }
