daa9dbc4269e956cbfd6ae25693f5d96742f9a48
Ilvian <d.ilvian1993@gmail.com>
2023-06-16 03:39:51 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h> 
+#include<stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n"); 
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
