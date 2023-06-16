ded8b9facaae9b0d28925d2e364f522d677d4ac6
Ilvian <d.ilvian1993@gmail.com>
2023-06-16 03:08:05 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main(){puts("Programming is like building a multilingual puzzle\n);return 0;}
+include<stdio.h> int main(){puts("Programming is like building a multilingual puzzle\n);return 0;}
