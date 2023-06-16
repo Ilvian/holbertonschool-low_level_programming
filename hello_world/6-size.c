5e62d21542628b0137c3f784d16f7651fe727968
Ilvian <d.ilvian1993@gmail.com>
2023-06-16 04:39:24 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,11 @@
  */
 int main(void)
 {
-	int a;long int b;long long int c;char d;float f;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
