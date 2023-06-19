c1fc7b91045a77f1e01e3f917718b559adec7851
Ilvian <d.ilvian1993@gmail.com>
2023-06-19 03:52:40 -0700

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,9 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++ && letter = 'A'; letter <= 'Z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+		for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 			putchar('\n');
 
