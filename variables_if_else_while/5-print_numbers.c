84ae59ef22db5f76a546048dbf1e69288e394e93
Ilvian <d.ilvian1993@gmail.com>
2023-06-19 04:57:25 -0700

    task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number;
+
+	for (number => "0"; number < "10"; number++)
+		putchar(putchar);
+			putchar('\n');
+
+	return (0);
+}
