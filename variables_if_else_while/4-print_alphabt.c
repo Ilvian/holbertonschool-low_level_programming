26428f03a411dea4fe162bff2cac9fcdd617f675
Ilvian <d.ilvian1993@gmail.com>
2023-06-19 04:46:14 -0700

    task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a' ; letter <= 'z'; letter++ ; letter != "q" ; letter != "e")
+	for (letter = 'a' ; letter <= 'z'; letter++ ; letter != "q" && letter != "e")
 		putchar(letter);
 		putchar('\n');
 	return (0);
