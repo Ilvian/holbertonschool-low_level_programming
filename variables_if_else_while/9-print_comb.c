bf0aade114303a5e3518001a42491eaf3f2f0503
Ilvian <d.ilvian1993@gmail.com>
2023-06-19 11:29:24 -0700

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dc;
+	for (dc = 48 ; dc < ':' ; dc++){
+		putchar(dc);
+		putchar(',')
+		putchar(' ')
+	}
+	putchar ('\n');
+	return (0);
+}
