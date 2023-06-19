9b221ea9214f8b18588a4998ed31b74b18d81816
Ilvian <d.ilvian1993@gmail.com>
2023-06-19 11:03:19 -0700

    task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Alph;
+
+	for (Alph = 122; Alph >= 97; Alph--){
+		putchar(Alph);
+	}
+			putchar('\n');
+
+	return (0);
+}
