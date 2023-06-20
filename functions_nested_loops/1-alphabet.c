460d5398ec981dfe3491d56727c941a579b23488
Ilvian <d.ilvian1993@gmail.com>
2023-06-20 02:53:14 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+			putchar('\n');
+
+	return (0);
+}
