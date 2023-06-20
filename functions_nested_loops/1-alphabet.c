d0ac6638a9aca1d20ad19cbcd98427c2184c7c53
Ilvian <d.ilvian1993@gmail.com>
2023-06-20 02:56:13 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,8 +10,8 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-			putchar('\n');
+		_putchar(letter);
+	_putchar('\n');
 
 	return (0);
 }
