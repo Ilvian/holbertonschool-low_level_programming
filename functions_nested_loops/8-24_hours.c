4d3adf7dbdaf33b20e058217f905689f68808990
Ilvian <d.ilvian1993@gmail.com>
2023-06-20 11:57:24 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int min;
+	int hr;
+
+		for (hr = 0; hr <= 23; hr++)
+		{
+			for (min = 0; min <= 59; min++)
+			{
+				_putchar((hr / 10) + '0');
+				_putchar((hr % 10) + '0');
+				-putchar (':');
+				_putchar((min / 10) + '0');
+				_putchar((min % 10) + '0');
+			}
+		}
+}
