d3335dffc5af5023dc1253d8c1cb6fbdeaf7d8b7
Ilvian <d.ilvian1993@gmail.com>
2023-06-20 12:48:09 -0700

    task 10

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			{
 				_putchar((hr / 10) + '0');
 				_putchar((hr % 10) + '0');
-				-putchar (':');
+				-putchar(':');
 				_putchar((min / 10) + '0');
 				_putchar((min % 10) + '0');
 			}
