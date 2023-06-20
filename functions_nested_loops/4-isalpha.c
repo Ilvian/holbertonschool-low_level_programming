42cb7486ff52cc1c139f85c8c47f6b2ff4bf58ad
Ilvian <d.ilvian1993@gmail.com>
2023-06-20 05:21:46 -0700

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+			else if (c >= 'A' && c <= 'Z')
+				return (1);
+	else
+		return (0);
+}
