9615a9654cc07155fc1cb4731a475c013a9f9c9c
Ilvian <d.ilvian1993@gmail.com>
2023-06-23 02:27:58 -0700

    tasks

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,16 +13,12 @@ void print_triangle(int size)
 		_putchar ('\n');
 	else
 		{
-			for (x = 0; x < size; x++)
+			for (x = 1; x <= size; x++)
 			{
-				for (y = x; y <= size; y++)
-				{
+				for (y = size - x; y > 0; y--)
 					_putchar (' ');
-				}
-				for (y = 0; y <= x; y++)
-				{
+				for (y = 0; y < x; y++)
 					_putchar ('#');
-				}
 				_putchar ('\n');
 			}
 		}
