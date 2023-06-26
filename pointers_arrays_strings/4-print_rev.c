ef4b3b07291b4813af8b54de0e4e77246abb3fae
Ilvian <d.ilvian1993@gmail.com>
2023-06-26 04:17:18 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - main function
+ * @s: param
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int r;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (r = l - 1; r > 0; r--)
+		_putchar (s[r]);
+	_putchar ('\n');
+}
