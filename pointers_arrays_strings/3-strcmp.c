4ab5af2682b35f1de08f408e14005aa6df235e70
Ilvian <d.ilvian1993@gmail.com>
2023-06-27 05:30:17 -0700

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 		i++;
-	for ((s1[i] - s2[i]) < 0)
+	if ((s1[i] - s2[i]) < 0)
 		return (s1[i] < s2[i]);
-	for ((s1[i] - s2[i]) > 0)
+	if ((s1[i] - s2[i]) > 0)
 		return (s1[i] > s2[i]);
-	for ((s1[i] - s2[i]) = 0)
+	if ((s1[i] - s2[i]) = 0)
                 return (s1[i] = s2[i]);
 }
