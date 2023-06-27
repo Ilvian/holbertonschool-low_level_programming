795310972b360183ed6ff6a631dbb606796628f3
Ilvian <d.ilvian1993@gmail.com>
2023-06-27 04:23:21 -0700

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - main function
+ * @dest: param
+ * @src: param
+ * @n: param
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
