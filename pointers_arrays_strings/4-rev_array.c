2335c4a53e331d322b82b66d852ae1ad68cd222d
Ilvian <d.ilvian1993@gmail.com>
2023-06-27 06:08:51 -0700

    task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *reverse_array -  function that reverses the content of an array of integers
+ *@a: pointer to array
+ *@n: n to be reversed
+*/
+void reverse_array(int *a, int n)
+{
+	int length = 0;
+	int reverse;
+
+	while (a[length] != '\0')
+		length++;
+	while (a[reverse] = a[length])
+		reverse--;
+}
