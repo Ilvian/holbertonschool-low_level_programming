e8f1e81c0b1ed02ffc2687b360126fa49aa49454
Ilvian <d.ilvian1993@gmail.com>
2023-06-29 01:36:59 -0700

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- *_memset -  function that copies a string
+ *_memset - fills memory with a constant byte
  *@s: address of memory to be filled
- *@b: th value
+ *@b: the value
  *@n: n bytes to be changed
  *Return: return copied string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-
-	for (; n > 0; i++)
+	for (; n > 0;i++)
 	{
-		s[i] - b;
+		s[i] = b;
 		n--;
 	}
 	return (s);
