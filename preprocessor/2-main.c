c5401a1892979f93aa77e5aedc5319e01fb75672
Ilvian <d.ilvian1993@gmail.com>
2023-07-13 02:30:35 -0700

    task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
