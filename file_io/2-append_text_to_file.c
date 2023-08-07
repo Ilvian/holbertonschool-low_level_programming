795baafd53be9b6f23b0a3f6ee39d01c65d9a494
Ilvian <d.ilvian1993@gmail.com>
2023-08-07 13:13:52 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,20 +8,22 @@
  * @text_content: input
  * Return: Return 1 if the file exists and -1 if the file does not exist
  */
-int append_text_to_file(const char *filename, char *text_content) {
+int append_text_to_file(const char *filename, char *text_content)
+{
 	int fd = open(filename, O_WRONLY | O_APPEND);
-    if (!filename) return -1;  
 
-    if (fd == -1) return -1;   
-
-    if (text_content != NULL) {
-        if (write(fd, text_content, strlen(text_content)) == -1) {  
-            close(fd);
-            return -1;   
-        }
-    }
-
-    close(fd);   
-    return 1;    
+	if (!filename)
+		return (-1);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
-
