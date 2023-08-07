796e1d28d983318315bde1c36bc11c9ba8c77e41
Ilvian <d.ilvian1993@gmail.com>
2023-08-07 13:09:00 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,34 +2,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
-
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: input
+ * @text_content: input
+ * Return: Return 1 if the file exists and -1 if the file does not exist
+ */
 int append_text_to_file(const char *filename, char *text_content) {
 	int fd = open(filename, O_WRONLY | O_APPEND);
-	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+    if (!filename) return -1;  
 
-    if(filename == NULL) {
-        return -1;
+    if (fd == -1) return -1;   
+
+    if (text_content != NULL) {
+        if (write(fd, text_content, strlen(text_content)) == -1) {  
+            close(fd);
+            return -1;   
+        }
     }
 
-    if(access(filename, F_OK) == -1 || access(filename, W_OK) == -1) {
-        return -1;
-    }
-
-    if(text_content == NULL) {
-        return 1;
-    }
-
-    if(fd == -1) {
-        return -1;
-    }
-
-    if(bytes_written == -1) {
-        close(fd);
-        return -1;
-    }
-
-    close(fd);
-    return 1;
+    close(fd);   
+    return 1;    
 }
 
