fde665120f5d32ebbff929d19bc70bec937805d9
Ilvian <d.ilvian1993@gmail.com>
2023-08-07 13:27:43 -0700

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,8 +5,8 @@
 
 #define BUFFER_SIZE 1024
 
-void error_and_exit(char *msg, int code) {
-    dprintf(2, "%s\n", msg);
+void error_and_exit(char *msg, char *file, int code) {
+    dprintf(2, "%s %s\n", msg, file);
     exit(code);
 }
 
@@ -15,18 +15,24 @@ int main(int argc, char *argv[]) {
     int fd_from, fd_to;
     ssize_t count;
 
-    if (argc != 3) error_and_exit("Usage: cp file_from file_to", 97);
+    if (argc != 3) error_and_exit("Usage:", "cp file_from file_to", 97);
 
     fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1) error_and_exit("Error: Can't read from file", 98);
+    if (fd_from == -1) error_and_exit("Error: Can't read from", argv[1], 98);
 
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1) error_and_exit("Error: Can't write to file", 99);
+    if (fd_to == -1) error_and_exit("Error: Can't write to", argv[2], 99);
 
-    while ((count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-        if (write(fd_to, buffer, count) != count) error_and_exit("Error: Can't write to file", 99);
+    while ((count = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        if (write(fd_to, buffer, count) != count) {
+            close(fd_from);
+            error_and_exit("Error: Can't write to", argv[2], 99);
+        }
+    }
 
-    if (close(fd_from) == -1 || close(fd_to) == -1) error_and_exit("Error: Can't close fd", 100);
+    if (count == -1) error_and_exit("Error: Can't read from", argv[1], 98);
+
+    if (close(fd_from) == -1 || close(fd_to) == -1) error_and_exit("Error: Can't close fd", "", 100);
 
     return (0);
 }
