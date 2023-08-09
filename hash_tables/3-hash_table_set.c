41602d1b787fc724523543374640bad744b421f5
Ilvian <d.ilvian1993@gmail.com>
2023-08-09 01:24:54 -0700

    task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: 1 if succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (!current_node->value)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
